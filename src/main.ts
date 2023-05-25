e87e3a7b85c375e293109f3898d1c05edafee677
AKW <2497744746@qq.com>
2023-05-25 10:15:54 +0800

    setup husky

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from "vue"
+import App from "./App.vue"
 
-createApp(App).mount("#app");
+createApp(App).mount("#app")
 
 const fun = () => {
-  console.log("没有格式化的代码");
-};
+  console.log("没有格式化的代码")
+}
