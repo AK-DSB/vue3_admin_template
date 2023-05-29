54a208f51055ab3ae4dc77414fba2e1e30ee5917
AKW <2497744746@qq.com>
2023-05-29 18:09:25 +0800

    路由组件过度效果

--- src/layout/main/index.vue
+++ src/layout/main/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <router-view></router-view>
+  <router-view #="{ Component }">
+    <transition name="fade">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 <script lang="ts" setup></script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.fade-enter-from {
+  opacity: 0;
+  transform: scale(0);
+}
+.fade-enter-active {
+  transition: all 1s;
+}
+.fade-enter-to {
+  opacity: 1;
+  transform: scale(1);
+}
+</style>
